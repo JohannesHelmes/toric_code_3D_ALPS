9a8c9d3c2cd77335a3bd9f602528ef3ffdecb630
Johannes Helmes <helmes@thp.uni-koeln.de>
2015-12-10 16:25:46 +0100

    ThermoInt.py gives nan

--- processing/MakeTasks_TCU.py
+++ processing/MakeTasks_TCU.py
@@ -1,28 +1,50 @@
 #!/bin/bash alpspython
 
-import sys,os
+import sys,os,time
 import pyalps,argparse
 import numpy as np
+import configparser
+
+config = configparser.ConfigParser()
 
 parser = argparse.ArgumentParser(description='Create input files for ALPS simulations of upright toric code models in 2 and 3 dimensions', epilog='(C) Johannes Helmes 2015')
+filegroup=parser.add_argument_group()
+
+filegroup.add_argument('paramfilename')#,type=file)
+
+paramgroup=parser.add_argument_group()
 
 
-parser.add_argument('--sweeps','-s', type=int,default=10000,help='Number of sweeps, default=10000')
-parser.add_argument('--therm','-t', type=int,default=1000,help='Number of thermalization steps, default=1000')
-parser.add_argument('--n','-n', type=int,default=2,help='Order of Renyi entropy, default=2')
-parser.add_argument('--length','-l', type=int,help='Lattice length',required=True)
-parser.add_argument('--lattice','-a', type=int,help='Lattice type, (0 = toric code, 1 = toric code fcr, 2 = toric code 3D), default = 2',default=2)
-parser.add_argument('--ExcType','-e', type=int,help='Type of Excitation, (1 = plaquette, 2 = vertex), default = 2',default=2)
+paramgroup.add_argument('--sweeps','-s', type=int,default=10000,help='Number of sweeps, default=10000')
+paramgroup.add_argument('--therm','-t', type=int,default=1000,help='Number of thermalization steps, default=1000')
+paramgroup.add_argument('--n','-n', type=int,default=2,help='Order of Renyi entropy, default=2')
+paramgroup.add_argument('--length','-l', type=int,help='Lattice length',required=True)
+paramgroup.add_argument('--lattice','-a', type=int,help='Lattice type, (0 = toric code, 1 = toric code fcr, 2 = toric code 3D), default = 2',default=2)
+paramgroup.add_argument('--ExcType','-e', type=int,help='Type of Excitation, (1 = plaquette, 2 = vertex), default = 2',default=2)
 
-parser.add_argument('--beta','-b', nargs='+',type=float,help='beta-1/T, range possible default=2*l')
-parser.add_argument('--magnetization','-m', nargs='+',type=float,default=[0.0],help='Magnetization (h), range possible, default=0.0')
-parser.add_argument('--infile','-i', help='Prefix of .in.xml')
-parser.add_argument('--geofile','-g', type=file, help='File containing the geometry of the increments')
-parser.add_argument('--partsize','-p',default=1000, type=int,help='Size subsimulation partition for better distribution on HPC, default=1000')
-parser.add_argument('--temper','-T',help='interpret beta as temperature',action='store_true')
-parser.add_argument('--verbose','-v',help='Print some extra stuff',action='store_true')
-parser.add_argument('--directory','-d',help='Directory of the parameter files, default= /scratch/helmes/simulations/"name of parent directory"/')
+paramgroup.add_argument('--beta','-b', nargs='+',type=float,help='beta-1/T, range possible default=2*l')
+paramgroup.add_argument('--magnetization','-m', nargs='+',type=float,default=[0.0],help='Magnetization (h), range possible, default=0.0')
+paramgroup.add_argument('--infile','-i', help='Prefix of .in.xml')
+paramgroup.add_argument('--geofile','-g', type=file, help='File containing the geometry of the increments')
+paramgroup.add_argument('--partsize','-p',default=1000, type=int,help='Size subsimulation partition for better distribution on HPC, default=1000')
+paramgroup.add_argument('--temper','-T',help='interpret beta as temperature',action='store_true')
+paramgroup.add_argument('--verbose','-v',help='Print some extra stuff',action='store_true')
+paramgroup.add_argument('--directory','-d',help='Directory of the parameter files, default= /scratch/helmes/simulations/"name of parent directory"/')
 args=parser.parse_args()
+
+if args.directory==None:
+    dirpath='/scratch/helmes/simulations/'+os.path.split(os.path.split(os.getcwd())[0])[1]
+    print dirpath
+else:
+    dirpath=args.directory
+
+config['DEFAULT'] = { 'sweeps': args.sweeps, 'therm': args.therm, 'n': args.n, 'length': args.length, 'lattice': args.lattice, 'ExcType':args.ExcType, 'beta': args.beta,
+        'magnetization': args.magnetization, 'infile': args.infile, 'geofile':args.geofile.name, 'partsize':args.partsize, 'temper':args.temper, 'directory':dirpath} 
+
+with open(args.paramfilename,'w') as configfile:
+    timestring=time.strftime("%Y-%m-%d %H:%M:%S \n\n", time.localtime())
+    configfile.write("# created on " + timestring)
+    config.write(configfile)
 
 beta=args.beta
 
@@ -37,9 +59,6 @@ if args.geofile==None:
 else:
     geo=os.path.abspath(args.geofile.name)
 
-if args.directory=='':
-    path='/scratch/helmes/simulations/'+os.path.split(os.path.split(os.getcwd())[0])
-    print path
 
 parms = []
 betagrid=beta if len(beta)==3 else np.array((beta[0],beta[0],1))
@@ -93,12 +112,13 @@ cuts=np.append(np.arange(0,len(parms),args.partsize),len(parms))
 print cuts
 #path=os.path.dirname(args.infile)
 
-if not os.path.exists(path):
-    os.makedirs(path)
+if not os.path.exists(dirpath):
+    os.makedirs(dirpath)
+    print dirpath,"created"
 
 for i,[s,e] in enumerate(zip(cuts[:-1],cuts[1:])):
     #fname=path+'/part'+str(i)+'/'+os.path.basename(args.infile)+'.'+str(i)
-    fname=path+'/'+os.path.basename(args.infile)+'.'+str(i)
+    fname=dirpath+'/'+os.path.basename(args.infile)+'.'+str(i)
     #os.system('mkdir -p '+path+'/part'+str(i))
     input_file = pyalps.writeInputFiles(fname,parms[s:e])
 
---
+++ processing/ThermoInt.py
@@ -0,0 +1,92 @@
+import matplotlib.pylab as plt
+import pyalps,argparse
+import os
+import plottery
+import numpy as np
+from scipy.integrate import simps, romb
+
+parser = argparse.ArgumentParser(description='Perform thermodynamic integration to get O(1) contribution from Renyi entropies', epilog='(C) Johannes Helmes 2014')
+
+parser.add_argument('--infile','-i', help='Prefix of result files',required=True)
+parser.add_argument('--X','-x',default='h',help='Parameter name, (default h)')
+parser.add_argument('--Y','-y',default='Magnetization',help='Parameter name, (default Magnetization)')
+parser.add_argument('--Const','-c',default=np.log(2),type=float,help='Value of the constant for the integration, default=ln(2)')
+parser.add_argument('--B','-b',default=1000,type=int,help='Number of Bootstrap samples, default=1000')
+parser.add_argument('--N','-n',default=2,type=int,help='Renyi order, default=2')
+parser.add_argument('--Dim','-d',default=2,type=int,help='Dimension of the system, default=2')
+parser.add_argument('--System','-s',default='toriccode',help="['toriccode','toriccode3D','ising','toriccodeupright']")
+parser.add_argument('--verbose','-v',action='store_true')
+parser.add_argument('--Simple','-S',action='store_true')
+parser.add_argument('--alternate','-a',action='store_true',help='Use this flag, if bipartition 2 and 3 are different, and 4 bipartitions are given')
+parser.add_argument('--Step','-e',default=2,type=int,help='Number of increment data points for thermodynamic integration, default=2')
+parser.add_argument('--outfile','-o',help='Filename of the result file. If not given, print results to stdout.')
+
+args=parser.parse_args()
+
+REntropy={}
+
+dataset = pyalps.loadMeasurements(pyalps.getResultFiles(prefix=args.infile),[args.Y])
+
+'''
+if args.verbose:
+    print args.infile
+    print datasets
+'''
+
+renyi_dataG = pyalps.collectXY(dataset, x=args.X, y=args.Y, foreach=['IncNo'])
+n=args.N
+
+
+if args.verbose:
+    print renyi_dataG
+
+factor={}
+factor['ising']=2
+factor['toriccode']=4
+factor['toriccodeupright']=2
+factor['toriccode3D']=3
+
+N=args.B
+
+
+if args.Simple:
+    S=np.zeros(N)
+    for k in range(3,len(renyi_dataG[0].x),args.Step):
+        beta=np.array(renyi_dataG[0].x)[:k]
+        y=np.array(renyi_dataG[0].y)[:k]
+        av_magn=np.array([float(str(y[i]).split(" ")[0]) for i in range(len(y))])
+        av_magn_err=np.array([float(str(y[i]).split(" ")[2]) for i in range(len(y))])
+
+        for i in range(N):
+            Bstrap_magn=[np.random.normal(av_magn[j],av_magn_err[j]) for j in range(len(y))]
+            S[i]=simps(Bstrap_magn,beta,even='avg')
+        print S[2]
+
+
+        gamma=1./(n-1)*np.array([args.Const-(n*factor[args.System]*renyi_dataG[0].props['L']**args.Dim)*S[l] for l in range(N)])
+
+        if args.outfile==None:
+            print beta[-1], np.mean(gamma), np.std(gamma)#, np.mean(SZ2), np.mean(S[0]), np.mean(S[1]), np.mean(S[2])
+            
+else:
+    S=np.zeros((5,N))
+
+    for k in range(3,len(renyi_dataG[0].x),args.Step):
+        beta=np.array(renyi_dataG[0].x)[:k]
+        for schemes in renyi_dataG:
+            scheme=int(schemes.props['IncNo'])
+            y=np.array(schemes.y)[:k]
+            av_magn=np.array([float(str(y[i]).split(" ")[0]) for i in range(len(y))])
+            av_magn_err=np.array([float(str(y[i]).split(" ")[2]) for i in range(len(y))])
+
+            for i in range(N):
+                Bstrap_magn=[np.random.normal(av_magn[j],av_magn_err[j]+0.001*abs(av_magn[j])) for j in range(len(y))]
+                S[scheme,i]=simps(Bstrap_magn,beta,even='avg')
+
+        if args.alternate:
+            gamma=np.array([args.Const-(2*factor[args.System]*schemes.props['L']**args.Dim)*(S[0,l]-S[1,l]-S[2,l]+S[3,l]) for l in range(N)])
+        else :
+            gamma=1./(n-1)*np.array([args.Const-(n*factor[args.System]*schemes.props['L']**args.Dim)*(S[0,l]-2*S[1,l]+S[2,l]) for l in range(N)])
+
+        if args.outfile==None:
+            print beta[-1], np.mean(gamma), np.std(gamma)#, np.mean(SZ2), np.mean(S[0]), np.mean(S[1]), np.mean(S[2])
