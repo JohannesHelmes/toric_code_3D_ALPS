94f1a541166a79b74093f89a6027d4d0db6837ef
Johannes Helmes <helmes@thp.uni-koeln.de>
2016-01-06 14:39:24 +0100

    better ThermoInt

--- processing/ThermoInt.py
+++ processing/ThermoInt.py
@@ -5,7 +5,7 @@ import plottery
 import numpy as np
 from scipy.integrate import simps, romb
 
-parser = argparse.ArgumentParser(description='Perform thermodynamic integration to get O(1) contribution from Renyi entropies', epilog='(C) Johannes Helmes 2014')
+parser = argparse.ArgumentParser(description='Perform thermodynamic integration to get O(1) contribution from Renyi entropies', epilog='(C) Johannes Helmes 2016')
 
 parser.add_argument('--infile','-i', help='Prefix of result files',required=True)
 parser.add_argument('--X','-x',default='h',help='Parameter name, (default h)')
@@ -14,16 +14,14 @@ parser.add_argument('--Const','-c',default=np.log(2),type=float,help='Value of t
 parser.add_argument('--B','-b',default=1000,type=int,help='Number of Bootstrap samples, default=1000')
 parser.add_argument('--N','-n',default=2,type=int,help='Renyi order, default=2')
 parser.add_argument('--Dim','-d',default=2,type=int,help='Dimension of the system, default=2')
-parser.add_argument('--System','-s',default='toriccode',help="['toriccode','toriccode3D','ising','toriccodeupright']")
+parser.add_argument('--System','-S',default='toriccode',choices=['toriccode','toriccode3D','ising','toriccodeupright'])
 parser.add_argument('--verbose','-v',action='store_true')
-parser.add_argument('--Simple','-S',action='store_true')
-parser.add_argument('--alternate','-a',action='store_true',help='Use this flag, if bipartition 2 and 3 are different, and 4 bipartitions are given')
+parser.add_argument('--alternate','-a',action='store_true',help="If bipartitions 2 and 3 are NOT identical")
+parser.add_argument('--simple','-s',type=int,help="With this flag, bipartition 0 is assumed to be empty, compute the entropy itself instead of gamma, provide the bipartition number > 0")
 parser.add_argument('--Step','-e',default=2,type=int,help='Number of increment data points for thermodynamic integration, default=2')
 parser.add_argument('--outfile','-o',help='Filename of the result file. If not given, print results to stdout.')
 
 args=parser.parse_args()
-
-REntropy={}
 
 path=os.path.dirname(args.infile)
 filename=os.path.basename(args.infile)
@@ -41,6 +39,10 @@ n=args.N
 if args.verbose:
     print renyi_dataG
 
+if args.outfile!=None:
+    outfile=open(args.outfile,'w')
+
+
 factor={}
 factor['ising']=2
 factor['toriccode']=4
@@ -49,51 +51,87 @@ factor['toriccode3D']=3
 
 N=args.B
 
+def bootstrap(avg,err,N=1000):
+    """ Performs a bootstrapping of an array of normal distributed data
 
-if args.Simple:
-    S=np.zeros(N)
-    for k in range(3,len(renyi_dataG[0].x),args.Step):
-        beta=np.array(renyi_dataG[0].x)[:k]
-        y=np.array(renyi_dataG[0].y)[:k]
-        av_magn=np.array([float(str(y[i]).split(" ")[0]) for i in range(len(y))])
-        av_magn_err=np.array([float(str(y[i]).split(" ")[2]) for i in range(len(y))])
+    Parameters
+    ----------
+    avg : array_like
+            Gaussian distributed average values
+    err : array_like
+            Errors on values
+    N   : int, optional
+            Number of bootstrapped values (default = 1000)
 
-        for i in range(N):
-            Bstrap_magn=[np.random.normal(av_magn[j],av_magn_err[j]) for j in range(len(y))]
-            S[i]=simps(Bstrap_magn,beta,even='even')
-        print beta
+    Returns
+    ---------
+    samples : ndarray
+            bootstrapped values, the result is an array of dimension avg.ndim + 1
+    """
+
+    #Needs numpy
+    if len(avg) != len(err):
+        raise ValueError("Arrays must be of the same dimension.")
+
+    return np.array( [ np.random.normal(avg[j],err[j],size=N) for j in range(len(avg)) ] )
 
 
-        gamma=1./(n-1)*np.array([args.Const-(n*factor[args.System]*renyi_dataG[0].props['L']**args.Dim)*S[l] for l in range(N)])
+def thermodynamic_integration(x,y,yerr,N=1000):
+    """ Performs the integration using Simpson's rule after bootstrapping the y values.
+    """
+    Q=np.zeros(N)
 
-        if args.outfile==None:
-            print beta[-1], np.mean(gamma), np.std(gamma)#, np.mean(SZ2), np.mean(S[0]), np.mean(S[1]), np.mean(S[2])
+    y_bstrap=bootstrap(y,yerr,N)
+    for i in xrange(N):
+        Q[i]=simps(y_bstrap[:,i],x,even='avg')
+
+    return Q
+    
+    
+
+def read_alps_data(alpsXY):
+    """ Reads x, y, y_err from ALPS dataset and returns them
+    """
+    x=np.array(alpsXY.x)
+    y_str=np.array(alpsXY.y)
+    y=np.array([float(str(y_str[i]).split(" ")[0]) for i in range(len(y_str))])
+    y_err=np.array([float(str(y_str[i]).split(" ")[2]) for i in range(len(y_str))])
+
+    return x,y,y_err
+
+
+NIncSteps=len(renyi_dataG)
+S=np.zeros((NIncSteps,N))   #entropies for all bootstrapped samples
+NValues=len(renyi_dataG[0].x)
+L=renyi_dataG[0].props['L']
+
+x=np.zeros((NIncSteps,NValues))
+y=np.zeros((NIncSteps,NValues))
+y_err=np.zeros((NIncSteps,NValues))
+
+for schemes in renyi_dataG:
+    scheme=int(schemes.props['IncNo'])
+    x[scheme],y[scheme],y_err[scheme] = read_alps_data(schemes)
+
+
+for k in range(3,NValues+1,args.Step):
+    for schemes in renyi_dataG:
+        scheme=int(schemes.props['IncNo'])
+        S[scheme]=thermodynamic_integration(x[scheme,:k],y[scheme,:k],y_err[scheme,:k],N)
+
+    if args.alternate:
+        gamma=np.array([args.Const-(2*factor[args.System]* L ** args.Dim) * (S[0,l]-S[1,l]-S[2,l]+S[3,l]) for l in range(N)])
+    elif args.simple:
+        gamma=np.array([args.Const-(2*factor[args.System]* L ** args.Dim) * (S[args.simple,l]-S[0,l]) for l in range(N)])
+    else:
+        gamma=1./(n-1)*np.array([args.Const-(n*factor[args.System] * L ** args.Dim) * (S[0,l]-2*S[1,l]+S[2,l]) for l in range(N)])
+
+
+    if args.outfile:
+        outfile.write("{0:f} {1:f} {2:f} \n".format(x[0,k-1], np.mean(gamma), np.std(gamma)) )
+    else:
+        print x[0,k-1], np.mean(gamma), np.std(gamma)
+
             
-else:
-    S=np.zeros((5,N))
-
-    if args.outfile!=None:
-        outfile=open(args.outfile,'w')
-    for k in range(3,len(renyi_dataG[0].x),args.Step):
-        beta=np.array(renyi_dataG[0].x)[:k]
-        for schemes in renyi_dataG:
-            scheme=int(schemes.props['IncNo'])
-            y=np.array(schemes.y)[:k]
-            av_magn=np.array([float(str(y[i]).split(" ")[0]) for i in range(len(y))])
-            av_magn_err=np.array([float(str(y[i]).split(" ")[2]) for i in range(len(y))])
-
-            for i in range(N):
-                Bstrap_magn=[np.random.normal(av_magn[j],av_magn_err[j]+0.001*abs(av_magn[j])) for j in range(len(y))]
-                S[scheme,i]=simps(Bstrap_magn,beta,even='avg')
-
-        if args.alternate:
-            gamma=np.array([args.Const-(2*factor[args.System]*schemes.props['L']**args.Dim)*(S[0,l]-S[1,l]-S[2,l]+S[3,l]) for l in range(N)])
-        else :
-            gamma=1./(n-1)*np.array([args.Const-(n*factor[args.System]*schemes.props['L']**args.Dim)*(S[0,l]-2*S[1,l]+S[2,l]) for l in range(N)])
-
-        if args.outfile==None:
-            print beta[-1], np.mean(gamma), np.std(gamma)#, np.mean(SZ2), np.mean(S[0]), np.mean(S[1]), np.mean(S[2])
-        else:
-            outfile.write("{0:f} {1:f} {2:f} \n".format(beta[-1], np.mean(gamma), np.std(gamma)) )
-    if args.outfile!=None:
-        outfile.close()
+if args.outfile!=None:
+    outfile.close()
